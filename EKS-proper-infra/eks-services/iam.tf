38ef2b8012609ed65e931f349208e1165e930898
Evatee-coder <adetayo.eyelade2@gmail.com>
2026-05-13 11:57:45 -0400

    fixed ALB IAM issue

--- EKS-proper-infra/eks-services/iam.tf
+++ EKS-proper-infra/eks-services/iam.tf
@@ -176,8 +176,10 @@ resource "aws_iam_policy" "aws_load_balancer_controller" {
         Action = [
           "elasticloadbalancing:CreateListener",
           "elasticloadbalancing:DeleteListener",
+          "elasticloadbalancing:ModifyListener",
           "elasticloadbalancing:CreateRule",
-          "elasticloadbalancing:DeleteRule"
+          "elasticloadbalancing:DeleteRule",
+          "elasticloadbalancing:ModifyRule"
         ]
         Resource = "*"
       },
